bb1b91ecc0a8f4b70b8e956d73371e5c29965a81
salahudeen abdulkareem <salayo4real@gmail.com>
2025-09-08 20:53:04 +0100

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function index() {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Welcome to the Listing App</h1>
-      <p className="mt-4 text-lg">Find the best listings in your area.</p>
+      <h1 className="text-4xl font-bold text-yellow-500">Welcome to the Listing App</h1>
+      <p className="mt-4 text-lg text-red-500">Find the best listings in your area.</p>
     </div>
   )
 }
